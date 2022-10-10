1c7b5c4a5b35699ed1ae3545922e7257e4e1a373
pengzhanbo <volodymyr@foxmail.com>
2022-10-11 05:58:57 +0800

    fix(eslint-config-ts): update import

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -1,9 +1,10 @@
+const config = require('./typescript')
 module.exports = {
   extends: [
     '@pengzhanbo/eslint-config-basic',
     'plugin:prettier/recommended',
     'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
-    './typescript',
   ],
+  ...config,
 }
