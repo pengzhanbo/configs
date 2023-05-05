7eecc17712f999af5d0b27db5099d787bbb677b0
pengzhanbo <volodymyr@foxmail.com>
2023-05-05 15:47:35 +0800

    build: bump deps

--- packages/eslint-config-ts/src/typescript.ts
+++ packages/eslint-config-ts/src/typescript.ts
@@ -1,4 +1,8 @@
 import basic from '@pengzhanbo/eslint-config-basic'
+import { existsSync } from 'node:fs'
+import { join } from 'node:path'
+
+const tsconfig = process.env.ESLINT_TSCONFIG || 'tsconfig.eslint.json'
 
 export default {
   settings: {
@@ -6,7 +10,52 @@ export default {
       node: { extensions: ['.js', '.jsx', '.mjs', '.ts', '.tsx', '.d.ts'] },
     },
   },
-  overrides: basic.overrides,
+  overrides: (basic.overrides as any).concat(
+    !existsSync(join(process.cwd(), tsconfig))
+      ? []
+      : [{
+          parserOptions: {
+            tsconfigRootDir: process.cwd(),
+            project: [tsconfig],
+          },
+          parser: '@typescript-eslint/parser',
+          excludedFiles: ['**/*.md/*.*'],
+          files: ['*.ts', '*.tsx', '*.mts', '*.cts'],
+          // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/recommended-requiring-type-checking.ts
+          rules: {
+            'no-throw-literal': 'off',
+            '@typescript-eslint/no-throw-literal': 'error',
+            'no-implied-eval': 'off',
+            '@typescript-eslint/no-implied-eval': 'error',
+            'dot-notation': 'off',
+            '@typescript-eslint/dot-notation': ['error', { allowKeywords: true }],
+            '@typescript-eslint/no-floating-promises': 'error',
+            '@typescript-eslint/no-misused-promises': 'error',
+            '@typescript-eslint/await-thenable': 'error',
+            '@typescript-eslint/no-for-in-array': 'error',
+            '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+            '@typescript-eslint/no-unsafe-argument': 'error',
+            '@typescript-eslint/no-unsafe-assignment': 'error',
+            '@typescript-eslint/no-unsafe-call': 'error',
+            '@typescript-eslint/no-unsafe-member-access': 'error',
+            '@typescript-eslint/no-unsafe-return': 'error',
+            'require-await': 'off',
+            '@typescript-eslint/require-await': 'error',
+            '@typescript-eslint/restrict-plus-operands': 'error',
+            '@typescript-eslint/restrict-template-expressions': 'error',
+            '@typescript-eslint/unbound-method': 'error',
+          },
+        }, {
+          // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/unbound-method.md
+          files: ['**/__tests__/**/*.ts', '**/*.spec.ts', '**/*.test.ts'],
+          plugins: ['jest'],
+          rules: {
+            // you should turn the original rule off *only* for test files
+            '@typescript-eslint/unbound-method': 'off',
+            'jest/unbound-method': 'error',
+          },
+        }]
+  ),
   rules: {
     'import/named': 'off',
 
@@ -73,8 +122,10 @@ export default {
     //     offsetTernaryExpressions: true,
     //   },
     // ],
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-invalid-this': 'off',
+    '@typescript-eslint/no-invalid-this': 'error',
+    // 'no-unused-vars': 'off',
+    // '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     'no-redeclare': 'off',
     '@typescript-eslint/no-redeclare': 'error',
     'no-use-before-define': 'off',
@@ -83,11 +134,7 @@ export default {
       { functions: false, classes: false, variables: true },
     ],
     'brace-style': 'off',
-    '@typescript-eslint/brace-style': [
-      'error',
-      '1tbs',
-      { allowSingleLine: true },
-    ],
+    '@typescript-eslint/brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
     'comma-dangle': 'off',
     '@typescript-eslint/comma-dangle': 'off',
     'object-curly-spacing': 'off',
@@ -156,7 +203,8 @@ export default {
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/no-namespace': 'off',
     '@typescript-eslint/triple-slash-reference': 'off',
+    // handled by unused-imports/no-unused-imports
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 }
