3fd10b0234a9ab2da2fb96d9ff2b122755ac3039
pengzhanbo <volodymyr@foxmail.com>
2024-01-22 00:03:21 +0800

    feat: update vue `no-extra-parens`

--- packages/eslint-config-vue/src/preset.ts
+++ packages/eslint-config-vue/src/preset.ts
@@ -1,10 +1,11 @@
 import type { PresetItem } from '@pengzhanbo/eslint-config'
-import { resolveSubOptions } from '@pengzhanbo/eslint-config'
+import { getOverrides, resolveSubOptions } from '@pengzhanbo/eslint-config'
 import { vue } from './vue'
 
 export const vuePreset: PresetItem = (options) => {
   return [vue({
     ...resolveSubOptions(options, 'vue'),
+    overrides: getOverrides(options, 'vue'),
     stylistic: options.stylistic,
     typescript: !!options.typescript,
   })]
