3e3fd23821ac1338f001b957a9abb33bc01b27d8
pengzhanbo <volodymyr@foxmail.com>
2026-08-13 14:51:10 +0800

    feat(eslint-config): update type gen

--- packages/eslint-config/src/typegen.d.ts
+++ packages/eslint-config/src/typegen.d.ts
@@ -3203,7 +3203,7 @@ export interface RuleOptions {
    */
   'node/no-new-require'?: Linter.RuleEntry<[]>
   /**
-   * disallow string concatenation with `__dirname` and `__filename`
+   * disallow string concatenation with `__dirname`, `__filename`, and `import.meta` paths
    * @see https://github.com/eslint-community/eslint-plugin-n/blob/HEAD/docs/rules/no-path-concat.md
    */
   'node/no-path-concat'?: Linter.RuleEntry<[]>
@@ -3313,10 +3313,20 @@ export interface RuleOptions {
    */
   'node/prefer-global/url-search-params'?: Linter.RuleEntry<NodePreferGlobalUrlSearchParams>
   /**
+   * enforce using `node:assert/strict` instead of `node:assert`.
+   * @see https://github.com/eslint-community/eslint-plugin-n/blob/HEAD/docs/rules/prefer-import/assert-strict.md
+   */
+  'node/prefer-import/assert-strict'?: Linter.RuleEntry<[]>
+  /**
    * enforce using the `node:` protocol when importing Node.js builtin modules.
    * @see https://github.com/eslint-community/eslint-plugin-n/blob/HEAD/docs/rules/prefer-node-protocol.md
    */
   'node/prefer-node-protocol'?: Linter.RuleEntry<NodePreferNodeProtocol>
+  /**
+   * enforce using `process.getBuiltinModule()` to load Node.js built-in modules
+   * @see https://github.com/eslint-community/eslint-plugin-n/blob/HEAD/docs/rules/prefer-process-get-builtin-module.md
+   */
+  'node/prefer-process-get-builtin-module'?: Linter.RuleEntry<NodePreferProcessGetBuiltinModule>
   /**
    * enforce `require("dns").promises`
    * @see https://github.com/eslint-community/eslint-plugin-n/blob/HEAD/docs/rules/prefer-promises/dns.md
@@ -13780,6 +13790,10 @@ type NodePreferGlobalUrl = []|[("always" | "never")]
 type NodePreferGlobalUrlSearchParams = []|[("always" | "never")]
 // ----- node/prefer-node-protocol -----
 type NodePreferNodeProtocol = []|[{
+  version?: string
+}]
+// ----- node/prefer-process-get-builtin-module -----
+type NodePreferProcessGetBuiltinModule = []|[{
   version?: string
 }]
 // ----- node/shebang -----
